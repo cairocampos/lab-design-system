b97c20c1e0d792caa5a24b239513c521ce0925af
Cairo <cairocampos98@gmail.com>
2022-10-12 21:20:50 -0300

    fix: config.base

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,9 +15,9 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  "viteFinal": (config, {configType}) => {
+  viteFinal: (config, {configType}) => {
     if(configType === "PRODUCTION") {
-      config.base = '/lab=design-system'
+      config.base = '/lab-design-system'
     }
     return config;
   }
